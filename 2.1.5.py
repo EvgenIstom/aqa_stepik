303022e5c6d711e2f41388c0532b5ec190db22d4
Evgenii Istomin <istom.evgen@gmail.com>
2023-04-17 14:24:32 +0300

    lesson 2.1.5

---
+++ 2.1.5.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import math
+
+try:
+    link = "https://suninjuly.github.io/math.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    def calc(x):
+        return str(math.log(abs(12*math.sin(int(x)))))
+
+    x = browser.find_element(By.ID, "input_value")
+    x = x.text
+    y = calc(x)
+
+    input1 = browser.find_element(By.ID, "answer")
+    input1.send_keys(y)
+
+    option1 = browser.find_element(By.ID, "robotCheckbox")
+    option1.click()
+    option2 = browser.find_element(By.ID, "robotsRule")
+    option2.click()
+
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button.click()
+
+finally:
+    time.sleep(5)
+    browser.quit()
--- lesson1.6.11.py
+++ lesson1.6.11.py
@@ -3,15 +3,15 @@ from selenium.webdriver.common.by import By
 import time
 
 try:
-    link = "http://suninjuly.github.io/registration2.html"
+    link = "http://suninjuly.github.io/registration1.html"
     browser = webdriver.Chrome()
     browser.get(link)
 
-    input1 = browser.find_element(By.CSS_SELECTOR, "input[placeholder='Input your first name']")
+    input1 = browser.find_element(By.CSS_SELECTOR, "[placeholder='Input your first name']")
     input1.send_keys("Ivan")
-    input2 = browser.find_element(By.CSS_SELECTOR, "input[placeholder='Input your last name']")
+    input2 = browser.find_element(By.CSS_SELECTOR, "[placeholder='Input your last name']")
     input2.send_keys("Petrov")
-    input3 = browser.find_element(By.CSS_SELECTOR, "input[placeholder='Input your email']")
+    input3 = browser.find_element(By.CSS_SELECTOR, "[placeholder='Input your email']")
     input3.send_keys("mail@mail.net")
 
     button = browser.find_element(By.CSS_SELECTOR, "button.btn")
