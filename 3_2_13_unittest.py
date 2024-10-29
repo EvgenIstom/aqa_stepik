e1ca49116679decf328d53fc5ce749047fb4abc0
EvgenIstom <istom.evgen@gmail.com>
2024-10-29 21:42:38 +0300

    unittest lesson

---
+++ 3_2_13_unittest.py
@@ -0,0 +1,54 @@
+from selenium import webdriver
+
+from selenium.webdriver.common.by import By
+import time
+import unittest
+
+
+class TestRegistration(unittest.TestCase):
+    def test_registration1(self):
+        link = "http://suninjuly.github.io/registration1.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
+
+        input1 = browser.find_element(By.CSS_SELECTOR, "[class='form-control first']")
+        input1.send_keys("Ivan")
+        input2 = browser.find_element(By.CSS_SELECTOR, "[class='form-control second']")
+        input2.send_keys("Petrov")
+        input3 = browser.find_element(By.CSS_SELECTOR, "[class='form-control third']")
+        input3.send_keys("mail@mail.net")
+
+        button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+        button.click()
+
+        time.sleep(1)
+
+        welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+        welcome_text = welcome_text_elt.text
+
+        self.assertEqual("Congratulations! You have successfully registered!", welcome_text, "Wrong welcome text")
+
+    def test_registration2(self):
+        link = "http://suninjuly.github.io/registration2.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
+
+        input1 = browser.find_element(By.CSS_SELECTOR, "[class='form-control first']")
+        input1.send_keys("Ivan")
+        input3 = browser.find_element(By.CSS_SELECTOR, "[class='form-control third']")
+        input3.send_keys("mail@mail.net")
+
+        button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+        button.click()
+
+        time.sleep(1)
+
+        welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+        welcome_text = welcome_text_elt.text
+
+        self.assertEqual("Congratulations! You have successfully registered!", welcome_text, "Wrong welcome text")
+
+
+if __name__ == "__main__":
+    unittest.main()
+
--- sandbox.py
+++ sandbox.py
@@ -1,14 +1,39 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+#
+# browser = webdriver.Chrome()
+# # говорим WebDriver ждать все элементы в течение 5 секунд
+# browser.implicitly_wait(5)
+#
+# browser.get("http://suninjuly.github.io/wait2.html")
+#
+# button = browser.find_element(By.ID, "verify")
+# button.click()
+# message = browser.find_element(By.ID, "verify_message")
+#
+# assert "successful" in message.text
 
-browser = webdriver.Chrome()
-# говорим WebDriver ждать все элементы в течение 5 секунд
-browser.implicitly_wait(5)
+# s = 'My name is Eugene'
+#
+# if 'name' in s:
+#     print('Substring found')
+# else:
+#     print('Substring not found')
+#
+# index = s.find('ene')
+# if index != -1:
+#     print(f"Substring found at index {index}")
+# else:
+#     print('Substring not found')
 
-browser.get("http://suninjuly.github.io/wait2.html")
 
-button = browser.find_element(By.ID, "verify")
-button.click()
-message = browser.find_element(By.ID, "verify_message")
 
-assert "successful" in message.text
+# def test_substring(full_string, substring):
+#     if substring not in full_string:
+#         print(f'expected "{substring}" to be substring of "{full_string}"')
+#
+# def test_substring(full_string, substring)
+
+import test_abs_project
+
+print(test_abs1())
