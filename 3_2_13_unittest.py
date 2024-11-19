6484866cb159fd5bd7e34a180c72bf66a9883b8c
EvgenIstom <istom.evgen@gmail.com>
2024-11-19 22:23:14 +0300

    some more lessons

--- 3_2_13_unittest.py
+++ 3_2_13_unittest.py
@@ -1,14 +1,17 @@
 from selenium import webdriver
 
 from selenium.webdriver.common.by import By
-import time
+from selenium.webdriver.chrome.options import Options
+
 import unittest
 
+chrome_options = Options()
+chrome_options.add_argument("--headless")
 
 class TestRegistration(unittest.TestCase):
     def test_registration1(self):
         link = "http://suninjuly.github.io/registration1.html"
-        browser = webdriver.Chrome()
+        browser = webdriver.Chrome(options=chrome_options)
         browser.get(link)
 
         input1 = browser.find_element(By.CSS_SELECTOR, "[class='form-control first']")
@@ -21,8 +24,6 @@ class TestRegistration(unittest.TestCase):
         button = browser.find_element(By.CSS_SELECTOR, "button.btn")
         button.click()
 
-        time.sleep(1)
-
         welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
         welcome_text = welcome_text_elt.text
 
@@ -30,18 +31,18 @@ class TestRegistration(unittest.TestCase):
 
     def test_registration2(self):
         link = "http://suninjuly.github.io/registration2.html"
-        browser = webdriver.Chrome()
+        browser = webdriver.Chrome(options=chrome_options)
         browser.get(link)
 
         input1 = browser.find_element(By.CSS_SELECTOR, "[class='form-control first']")
         input1.send_keys("Ivan")
+        input2 = browser.find_element(By.CSS_SELECTOR, "[class='form-control second']")
+        input2.send_keys("Petrov")
         input3 = browser.find_element(By.CSS_SELECTOR, "[class='form-control third']")
         input3.send_keys("mail@mail.net")
 
         button = browser.find_element(By.CSS_SELECTOR, "button.btn")
         button.click()
-
-        time.sleep(1)
 
         welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
         welcome_text = welcome_text_elt.text
